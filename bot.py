85bccc9a45409a1c299c3878f1a819619afeb44b
Infinity101-jpg <68457668+Infinity101-jpg@users.noreply.github.com>
2020-09-04 19:43:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,16 @@
 def DiscordLang():
-    (write) = print;import discord;from discord.ext import commands;
-    (write('Bot is starting up...'))
-    bot = commands.Bot(command_prefix = '')
-    client = bot
-    _ = bot
-
-
+    (write) = print;import discord;from discord.ext import commands;(write('Bot is starting up...'));bot = commands.Bot(command_prefix = '');client = bot;_ = bot;
     @client.event
-    async def on_ready():
-        (write('Bot is Online'));
-
-        
-    token = 'NzUxMzMxNjI5Mzk3NTczNjYy.X1HiGw.jW4_AtFui26x8NVUPppfTWrsWSU';
+    async def on_ready():(write('Bot is Online'));
+    token = 'NzM0NDAwNTQ2MzI4NDEyMjcx.XxRJzg.mdtGHQOGh8_QnsQy_BFdaGWtPr8';
+############################################################################################################################################################################################
 
 
 
 
 
     @_.command()
-    async def _helpa(_):
+    async def _help(_):
         await _.send('this is the help menu')
 
 
