f498a162661d0014c89d5965bcda1c49b0bbfdfb
Infinity101-jpg <68457668+Infinity101-jpg@users.noreply.github.com>
2020-09-04 20:33:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
-def DiscordLang():
-    (write) = print;import discord;from discord.ext import commands;(write('Bot is starting up...'));bot = commands.Bot(command_prefix = '');client = bot;_ = bot;
-    @client.event
-    async def on_ready():(write('Bot is Online'));
-    token = 'NzM0NDAwNTQ2MzI4NDEyMjcx.XxRJzg.mdtGHQOGh8_QnsQy_BFdaGWtPr8';
+(write) = print;import discord;from discord.ext import commands;(write('Bot is starting up...'));bot = commands.Bot(command_prefix = '');client = bot;_ = bot;
+@client.event
+async def on_ready():(write('Bot is Online'));
+token = 'NzUxNTA2NjUyNTE1MjcwODA2.X1KFHA.CzRiWVBR9QHqiW4-gQW4bBEui3Q';
 ############################################################################################################################################################################################
 
 
 
 
 
-    @_.command()
-    async def _help(_):
-        await _.send('this is the help menu')
+    
+@_.command()
+async def _ping(_):
+	await _.send('Pong!')
 
 
 
@@ -19,5 +19,6 @@ def DiscordLang():
 
 
 
-    bot.run(token)
+
+bot.run(token)
 
